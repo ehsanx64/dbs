80ce2f8e38a808d1368ea2bbc5db52a548b6c25e
Ehsan Mohammadi <ehsanx64@gmail.com>
2024-01-26 17:47:53 +0330

    mariadb: add the folder and a simple script

---
+++ mariadb/basic/persons-table.sql
@@ -0,0 +1,5 @@
+CREATE TABLE persons (
+    id bigint(20) NOT NULL AUTO_INCREMENT,
+    name varchar(100) NOT NULL,
+    PRIMARY KEY (id)
+) ENGINE=InnoDB CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
